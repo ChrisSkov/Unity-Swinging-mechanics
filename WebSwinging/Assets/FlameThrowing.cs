286effdbde9f2f77216edbf6bd975d97f4de7ad2
Chris <Stofferdyr@gmail.com>
2019-11-23 01:23:56 +0100

    create flamethrower in new scene

---
+++ WebSwinging/Assets/FlameThrowing.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameThrowing : MonoBehaviour
+{
+    [SerializeField] GameObject flameObj;
+    bool flameIsActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flameObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        ActivateFlame();
+    }
+    void ActivateFlame()
+    {
+
+        // if(!flameIsActive && Input.GetKeyDown(KeyCode.Q))
+        // {
+        //     flameObj.SetActive(true);//TODO: Refactor repeating code
+        //     flameIsActive = true;
+        //     print("eow");
+        // }
+        // else if(Input.GetKeyDown(KeyCode.Q))
+        // {
+        //     flameObj.SetActive(false);
+        //     flameIsActive = false;
+        // }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            flameObj.SetActive(true);//TODO: Refactor repeating code
+           // flameIsActive = true;
+            print("eow");
+        }
+        else if(Input.GetKeyUp(KeyCode.Q))
+        {
+            flameObj.SetActive(false);
+            print("ses");
+        }
+
+    }
+}
---
+++ WebSwinging/Assets/PlayerMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float groundCheckDistance = 1f;
+    Rigidbody rb;
+    public bool isGrounded = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, groundCheckDistance))
+        {
+            return true;
+
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, Vector3.down * groundCheckDistance);
+        Gizmos.color = Color.red;
+    }
+}
